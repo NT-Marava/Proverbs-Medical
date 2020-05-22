82aac6d8a4966ae013a15010ad58b73726df6ea5
Ned Marava <34473931+NT-Marava@users.noreply.github.com>
2020-05-22 09:14:28 +0200

    Add files via upload

---
+++ assets/js/custom.js
@@ -0,0 +1,72 @@
+
+/* jQuery Pre loader
+ -----------------------------------------------*/
+// $(window).load(function(){
+//     $('.preloader').fadeOut(1000); 
+// });
+
+
+/* Mobile Navigation
+    -----------------------------------------------*/
+// $(window).scroll(function() {
+//     if ($(".navbar").offset().top > 50) {
+//         $(".navbar-fixed-top").addClass("top-nav-collapse");
+//     } else {
+//         $(".navbar-fixed-top").removeClass("top-nav-collapse");
+//     }
+// });
+
+
+/* HTML document is loaded. DOM is ready. 
+-------------------------------------------*/
+$(document).ready(function () {
+
+  /* Hide mobile menu after clicking on a link
+    -----------------------------------------------*/
+  // $('.navbar-collapse a').click(function(){
+  //     $(".navbar-collapse").collapse('hide');
+  // });
+
+
+  /* Parallax section
+     -----------------------------------------------*/
+  // function initParallax() {
+  //   $('#intro').parallax("100%", 0.1);
+  //   $('#overview').parallax("100%", 0.3);
+  //   $('#detail').parallax("100%", 0.2);
+  //   $('#video').parallax("100%", 0.3);
+  //   $('#services').parallax("100%", 0.1);
+  //   $('#gallery').parallax("100%", 0.2);
+  //   $('#contact-us').parallax("100%", 0.1);
+  //   $('#faq').parallax("100%", 0.3);
+  //   $('#venue').parallax("100%", 0.1);
+  //   $('#sponsors').parallax("100%", 0.3);
+  //   $('#contact').parallax("100%", 0.2);
+
+  // }
+  // initParallax();
+
+
+  /* Owl Carousel
+  -----------------------------------------------*/
+  $(document).ready(function () {
+    $("#owl-services").owlCarousel({
+      autoPlay: 6000,
+      items: 4,
+      itemsDesktop: [1199, 2],
+      itemsDesktopSmall: [979, 1],
+      itemsTablet: [768, 1],
+      itemsTabletSmall: [985, 2],
+      itemsMobile: [479, 1],
+    });
+  });
+
+
+
+
+  /* wow
+  -------------------------------*/
+  new WOW({ mobile: false }).init();
+
+});
+
